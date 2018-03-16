8688073c69cff9560b8078bb35d364bea9f68f2f
Rishabh Shukla <rishabhshukla25@hotmail.com>
2018-03-16 13:20:25 +0530

    Removed unnecessary files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rishabhshukla/Documents/projects/smart-india-hack/Backend/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
