2f1fd83842ef39a869cfdb4bf34bb35e67672052
Rishabh Shukla <rishabhshukla25@hotmail.com>
2018-03-16 12:57:28 +0530

    Project setup

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/rishabhshukla/Documents/projects/smart-india-hack/Backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
